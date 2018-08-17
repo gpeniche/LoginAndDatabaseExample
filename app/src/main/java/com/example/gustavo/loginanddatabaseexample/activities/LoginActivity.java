75bc3d1c21a569303983240ee4fbb2c3cba8e01b
gustavo <yeniceri91@gmail.com>
2018-08-17 22:25:12 +0300

    Handle signup and write to db

--- app/src/main/java/com/example/gustavo/loginanddatabaseexample/activities/LoginActivity.java
+++ app/src/main/java/com/example/gustavo/loginanddatabaseexample/activities/LoginActivity.java
@@ -1,15 +1,156 @@
 package com.example.gustavo.loginanddatabaseexample.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.gustavo.loginanddatabaseexample.R;
+import com.example.gustavo.loginanddatabaseexample.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mLogInButton;
+    private Button mSignUpButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        // Views
+        mEmailField = findViewById(R.id.field_email);
+        mPasswordField = findViewById(R.id.field_password);
+        mLogInButton = findViewById(R.id.button_log_in);
+        mSignUpButton = findViewById(R.id.button_sign_up);
+
+        // Click listeners
+        mLogInButton.setOnClickListener(handleLogIn());
+        mSignUpButton.setOnClickListener(handleSignUp());
+    }
+
+    private View.OnClickListener handleLogIn() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn();
+            }
+        };
+    }
+
+    private View.OnClickListener handleSignUp() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        };
+    }
+
+    private void logIn() {
+        Log.d(TAG, "logIn");
+        if (!validateForm()) {
+            return;
+        }
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signUp() {
+        Log.d(TAG, "signUp");
+        if (!validateForm()) {
+            return;
+        }
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUser:onComplete:" + task.isSuccessful());
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Sign Up Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+
+        // Write new user
+        writeNewUser(user.getUid(), user.getEmail());
+
+        // Go to MainActivity
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+    }
+
+    private void writeNewUser(String userId, String email) {
+        User user = new User(email);
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(mEmailField.getText().toString())) {
+            mEmailField.setError("Required");
+            result = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        if (TextUtils.isEmpty(mPasswordField.getText().toString())) {
+            mPasswordField.setError("Required");
+            result = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return result;
     }
 }
